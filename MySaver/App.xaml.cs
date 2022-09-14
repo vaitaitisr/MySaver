c89af08ba24c003f81f9582d5c3579eeffef5c6c
Romualdas <90207669+RomasVait@users.noreply.github.com>
2022-09-14 11:44:30 +0300

    init front

---
+++ MySaver/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Platform;
+using MySaver.Handlers;
+
+namespace MySaver;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//Borderless entry
+		Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderlessEntry), (handler, view) =>
+		{
+			if(view is BorderlessEntry)
+			{
+#if ANDROID
+				handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+#endif
+			}
+		});
+
+		MainPage = new AppShell();
+	}
+}
