5a87eb522256e1cecf7eeab086470361de861a17
armandjas <armandas.jasilionis@gmail.com>
2022-09-27 15:10:20 +0300

    Removed unnecessary if/else statement from DataClass

--- MySaver/Controls/DataClass.cs
+++ MySaver/Controls/DataClass.cs
@@ -36,15 +36,8 @@ namespace MySaver.Controls
             input = input.ToLower();
 
             string[] resultList = Array.FindAll(productList, element => element.Contains(input));
-
-            if (resultList == null)
-            {
-                return null;
-            }
-            else
-            {
-                return resultList;
-            }
+			
+            return resultList;
         }
     }
 }
