b9c818447c4d691fd32e16c840be5b9bbbb578b7
Aleksandr <113241847+absplov@users.noreply.github.com>
2022-10-12 12:33:50 +0300

    Fix indentation

--- MySaver/MauiProgram.cs
+++ MySaver/MauiProgram.cs
@@ -6,22 +6,22 @@ namespace MySaver;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
             .ConfigureSyncfusionCore()
             .ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		builder.Services.AddSingleton<StoreService>();
-		builder.Services.AddSingleton<StoresViewModel>();
+        builder.Services.AddSingleton<StoreService>();
+        builder.Services.AddSingleton<StoresViewModel>();
         builder.Services.AddSingleton<StoresPage>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
