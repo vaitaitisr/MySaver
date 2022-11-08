7479cbdc2cf87921d4613e2f52dcdb5a2ffbc3b6
Aleksandr <113241847+absplov@users.noreply.github.com>
2022-11-08 21:48:18 +0200

    Update Product class to implement IComparable

--- MySaver/Models/Product.cs
+++ MySaver/Models/Product.cs
@@ -1,9 +1,14 @@
 ﻿namespace MySaver.Models;
 
-public class Product //Should probably implement IEquatable for better comparison functionality
+public class Product : IComparable<Product>
 {
     public string StoreName { get; set; }
     public string Name { get; set; }
     public float Amount { get; set; } = 1;
     public float Price { get; set; }
+
+    public int CompareTo(Product other)
+    {
+        return Price.CompareTo(other.Price);
+    }
 }
