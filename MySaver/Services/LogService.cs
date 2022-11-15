baf36ae9f4c1a66b3bd897c9043aff245dd821af
Aleksandr <113241847+absplov@users.noreply.github.com>
2022-11-15 22:02:52 +0200

    Log unhandled exceptions

---
+++ MySaver/Services/LogService.cs
@@ -0,0 +1,24 @@
+﻿namespace MySaver.Services;
+
+static class LogService
+{
+    private static string path;
+
+    static LogService()
+    {
+#if WINDOWS
+        path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+#elif ANDROID
+        path = "/storage/emulated/0/Documents";
+#endif
+
+        path = Path.Combine(path, "MySaver");
+        Directory.CreateDirectory(path);
+        path = Path.Combine(path, DateTime.Now.ToString("yyMMdd_HHmmss") + ".txt");
+    }
+
+    public static void Log(String message)
+    {
+        File.AppendAllText(path, $"{DateTime.Now.ToString("HH:mm:ss")} {message}\n");
+    }
+}
